4a68b32d316bd5ff57597f2041f4f3133133bfb5
Aslanbek <saidtahaev@gmail.com>
2022-11-26 23:12:45 +0300

    lets2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import FavouritesPage from './pages/FavouritesPage';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return <h1 className='font-bold'>hello react</h1>;
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/favourites' element={<FavouritesPage />} />
+    </Routes>
+  );
 }
 
 export default App;
